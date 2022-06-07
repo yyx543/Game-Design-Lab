76b87d4878908eefcca8b208faa6e96203161d97
Yi Xian <yyx543@gmail.com>
2022-06-07 21:50:00 +0800

    glowing shirt and breakable brick

---
+++ Assets/Scripts/Debris.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour
+{
+    private  Rigidbody2D rigidBody;
+    private  Vector3 scaler;
+    private  Vector2 currentPosition;
+
+    // Start is called before the first frame update
+    void  Start()
+    {
+        // we want the object to have a scale of 0 (disappear) after 30 frames. 
+        scaler  =  transform.localScale  / (float) 30 ;
+        rigidBody  =  GetComponent<Rigidbody2D>();
+        StartCoroutine("ScaleOut");
+    }
+
+    // void Update()
+    // {
+    //     currentPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+    //     Vector2 nextPosition = currentPosition;
+    //     rigidBody.MovePosition(nextPosition);
+    // }
+
+    IEnumerator  ScaleOut(){
+
+        Vector2 direction =  new  Vector2(Random.Range(-1.0f, 1.0f), 1);
+        rigidBody.AddForce(direction.normalized  *  10, ForceMode2D.Impulse);
+        rigidBody.AddTorque(10, ForceMode2D.Impulse);
+        // wait for next frame
+        yield  return  null;
+
+        // render for 0.5 second
+        for (int step =  0; step  < 30; step++)
+        {
+            this.transform.localScale  =  this.transform.localScale  -  scaler;
+            // wait for next frame
+            yield  return  null;
+        }
+
+        Destroy(gameObject);
+
+    }
+}
